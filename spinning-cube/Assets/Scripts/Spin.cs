81684b8fc1da64b08535ac272a23c37a44c16a1a
Benjamin Chan <benchan_2000@hotmail.com>
2018-11-30 16:09:22 +0000

    Added simple start/stop Button

--- spinning-cube/Assets/Scripts/Spin.cs
+++ spinning-cube/Assets/Scripts/Spin.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spin : MonoBehaviour
 {
+    public Text buttonText;
+    private bool _spin = false;
 
+   
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        if(_spin)
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    public void ButtonPressed()
+    {
+        _spin = !_spin;
+        buttonText.text = _spin == true ? "STOP" : "SPIN";
     }
 }
